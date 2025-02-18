7dbdc5d20f81378b632cbc9dfa87f90c0b33b550
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2025-02-18 20:21:39 -0300

    feat: atualiza estilos globais e adiciona novos componentes de UI

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+
+const Products = () => {
+  return (
+    <div>
+      Products
+      <Button> Teste</Button>
+    </div>
+  );
+};
+
+export default Products;
