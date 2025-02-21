f0a8f57ebd0982a6e11617a9c13698a74ca8bee1
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2025-02-20 23:34:17 -0300

    feat: adiciona restrição única ao campo slug da tabela Restaurant e configura cliente Prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
